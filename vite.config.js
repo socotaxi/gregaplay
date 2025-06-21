ca5f518a8eecc2114c3628bcda5432bc7068fef4
Edhem <edhemrombhot@gmail.com>
2025-06-21 16:19:50 +0100

    Début où l'Appli s'affiche correctement

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    // Make environment variables available to the app
     'process.env': process.env
-  }
+  },
+  server: {
+  port: 3000, // Essayez ce port
+  host: '127.0.0.1',
+  strictPort: false,
+}
 })
